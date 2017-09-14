49637b76699ca989584439f42e160419f6fe0caa
小草 <zuoge85@gmail.com>
2017-09-14 23:52:16 +0800

    加入初始房卡数

--- server/project/mj-boss/src/main/java/game/boss/services/SettingService.java
+++ server/project/mj-boss/src/main/java/game/boss/services/SettingService.java
@@ -43,6 +43,7 @@ public final class SettingService {
             setting = new SettingDO();
             setting.setId(DEFAULT_ID);
         }
+        setting.setInitGold(100);
         this.setting = setting;
     }
 
